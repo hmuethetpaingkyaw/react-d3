85a92662f2e6d3697d968b9237ffcad25ef27d8b
Hmue Thet Paing Kyaw <94979276+hmuethetpaingkyaw@users.noreply.github.com>
2023-06-25 14:28:00 +0630

    add my custom bar chart

--- src/viz/BarChart.jsx
+++ src/viz/BarChart.jsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from "react";
-import { csv, scaleBand, scaleLinear, max } from "d3";
+import React, { useState, useEffect } from "react";
+import { csv, scaleBand, scaleLinear} from "d3";
+import * as d3 from 'd3';
 
 const csvUrl =
   "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
 
-const width = 960;
+const width = 1160;
 const height = 500;
-const margin = { top: 20, right: 20, bottom: 20, left: 10 };
+const margin = { top: 20, right: 200, bottom: 20, left: 200 };
 
 const BarChart = () => {
   const [data, setData] = useState(null);
@@ -27,51 +28,31 @@ const BarChart = () => {
   const innerHeight = height - margin.top - margin.bottom;
   const innerWidth = width - margin.left - margin.right;
 
-  const yScale = scaleBand()
-    .domain(data.map((d) => d.Country))
-    .range([0, innerHeight]);
+  const yScale = scaleLinear()
+    .domain(d3.extent(data, (d)=> d.Population))
+    .range([innerHeight, 0]);
 
-  const xScale = scaleLinear()
-    .domain([0, max(data, (d) => d.Population)])
+  const xScale = scaleBand()
+    .domain(data.map((d)=> d.Country))
     .range([0, innerWidth]);
 
   return (
     <div
-      style={{
-        backgroundColor: "lightblue",
-      }}
     >
       <svg width={width} height={height}>
-        <g transform={`translate(${margin.left},${margin.top})`}>
-          {xScale.ticks().map((tickValue) => (
-            <g key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
-              <line y2={innerHeight} stroke="black" />
-              <text
-                style={{ textAnchor: "middle" }}
-                dy=".71em"
-                y={innerHeight + 3}
-              >
-                {tickValue}
-              </text>
-            </g>
-          ))}
-          {yScale.domain().map((tickValue) => (
-            <text
-              key={tickValue}
-              style={{ textAnchor: "end" }}
-              x={-3}
-              dy=".32em"
-              y={yScale(tickValue) + yScale.bandwidth() / 2}
-            >
-              {tickValue}
-            </text>
-          ))}
+        <g transform={`translate(${margin.left},${margin.top})`}> 
+        <AxisBottom xScale={xScale} yScale={yScale} />
+        <AxisLeft yScale={yScale} />
+          
           {data.map((d) => (
             <rect
               key={d.Country}
-              y={yScale(d.Country)}
-              width={xScale(d.Population)}
-              height={yScale.bandwidth()}
+              x={xScale(d.Country)}
+              y={yScale(d.Population)}
+              height={innerHeight - yScale(d.Population)}
+              width={xScale.bandwidth()}
+              fill="#69b3a2"
+              opacity={0.5}
             />
           ))}
         </g>
@@ -81,3 +62,71 @@ const BarChart = () => {
 };
 
 export default BarChart;
+
+// eslint-disable-next-line react/prop-types
+const AxisBottom = ({xScale, yScale}) => {
+  // eslint-disable-next-line react/prop-types
+  const [xStart, xEnd] = xScale.range();
+  // eslint-disable-next-line react/prop-types
+  const [yStart, yEnd] = yScale.range();
+  // eslint-disable-next-line react/prop-types
+  const ticks = xScale.domain();
+
+  return <> 
+         <line x1={xStart} x2={xEnd} y1={yStart} y2={yStart} stroke="blue" />
+  <g className="ticks">
+          {ticks.map((t, i) => {
+            const x = xScale(t);
+            return (
+              <React.Fragment key={i}>
+                <line x1={x} x2={x} y1={yStart} y2={yStart + 5} stroke="black" />
+                <text
+                  x={x}
+                  y={yStart + 20}
+                  fill="red"
+                  textAnchor="middle"
+                  fontSize={10}
+                >
+                  {t}
+                </text>
+              </React.Fragment>
+            );
+          })}
+        </g>
+  </>
+
+}
+
+// eslint-disable-next-line react/prop-types
+const AxisLeft = ({yScale}) => {
+  // eslint-disable-next-line react/prop-types
+  const [yStart, yEnd] = yScale.range();
+  // eslint-disable-next-line react/prop-types
+  const ticks = yScale.ticks();
+
+  return <> 
+  <line y1={yEnd} y2={yStart} stroke="blue" /> 
+  <g className="ticks">
+          {ticks.map((t, i) => {
+            const y = yScale(t);
+            return (
+              <React.Fragment key={i}>
+                <line x1={-5} y1={y} y2={y} stroke="black" />
+                <text
+                 x={-30}
+                  y={y}
+                  fill="red"
+                  textAnchor="middle"
+                  fontSize={10}
+                >
+                  {t}
+                </text>
+              </React.Fragment>
+            );
+          })}
+        </g>
+  </>
+
+}
+
+
